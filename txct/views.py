b76be3f87cc0a3cae374ed50398e9b9b8509594e
moixmail <gelabert.guillem@gmail.com>
2017-06-10 14:10:14 +0200

    added test content

--- txct/views.py
+++ txct/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Text
 
 def text_list(request):
-    return render(request, "txct/text_list.html", {})
+    texts = Text.objects.order_by('read_time')
+    return render(request, 'txct/text_list.html', {'texts': texts})
