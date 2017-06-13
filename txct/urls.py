1a40f31020d4cd7d1c562fa93a7e57d3348c33db
moixmail <gelabert.guillem@gmail.com>
2017-06-13 17:11:27 +0200

    added main functionality to reading time buttons

--- txct/urls.py
+++ txct/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.text_list, name='text_list'),
     url(r'^text/(?P<pk>\d+)/$', views.text_detail, name='text_detail'),
+    url(r'^cinc/$', views.text_5, name='cinc'),
+    url(r'^deu/$', views.text_10, name='deu'),
+    url(r'^quinze/$', views.text_15, name='quinze'),
+    url(r'^vint/$', views.text_20, name='vint'),
+    url(r'^vinticinc/$', views.text_25, name='vinticinc'),
 ]
--- txct/views.py
+++ txct/views.py
@@ -1,7 +1,9 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Text
 from django.shortcuts import render, get_object_or_404
+
 
 def text_list(request):
     texts = Text.objects.order_by('read_time')
@@ -11,6 +13,22 @@ def text_detail(request, pk):
     text = get_object_or_404(Text, pk=pk)
     return render(request, 'txct/text_detail.html', {'text': text})
 
-def text_5(request, pk):
-    text = get_object_or_404(Text, pk=pk)
-    return render(request, "txct/text_5.html", {'text': text})
+def text_5(request):
+    texts = Text.objects.filter(Q(read_time__gt=0) & Q(read_time__lte=6)).order_by("?")[0]
+    return render(request, 'txct/text_5.html', {'texts': texts})
+
+def text_10(request):
+    texts = Text.objects.filter(Q(read_time__gt=6) & Q(read_time__lte=12)).order_by("?")[0]
+    return render(request, 'txct/text_10.html', {'texts': texts})
+
+def text_15(request):
+    texts = Text.objects.filter(Q(read_time__gt=12) & Q(read_time__lte=16)).order_by("?")[0]
+    return render(request, 'txct/text_15.html', {'texts': texts})
+
+def text_20(request):
+    texts = Text.objects.filter(Q(read_time__gt=16) & Q(read_time__lte=22)).order_by("?")[0]
+    return render(request, 'txct/text_20.html', {'texts': texts})
+
+def text_25(request):
+    texts = Text.objects.filter(Q(read_time__gt=22) & Q(read_time__lte=35)).order_by("?")[0]
+    return render(request, 'txct/text_25.html', {'texts': texts})
