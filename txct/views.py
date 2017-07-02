864ded3284660e3b7afa5d75675c6211033e5a74
moixmail <gelabert.guillem@gmail.com>
2017-07-02 22:29:21 +0200

    Random was too random: pagination was implemented instead to cycle through all the texts of a certain length; text_# views were unified unused views were deleted

--- txct/views.py
+++ txct/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from .models import Text
 from django.shortcuts import render, get_object_or_404
 import json
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def base(request):
     context = {
@@ -12,34 +13,75 @@ def base(request):
     }
     return render(request, "txct/base.html", context)
 
-def text_list(request):
-    texts = Text.objects.order_by('read_time')
-    return render(request, 'txct/text_list.html', {'texts': texts})
-
 def index(request):
     return render(request, 'txct/index.html')
 
-def text_detail(request, pk):
-    text = get_object_or_404(Text, pk=pk)
-    return render(request, 'txct/text_detail.html', {'text': text})
-
 def text_5(request):
-    texts = Text.objects.filter(Q(read_time__gt=0) & Q(read_time__lte=6)).order_by("?")[0]
-    title = json.dumps(texts.title)
-    return render(request, 'txct/text_5.html', {'texts': texts})
+    texts = Text.objects.filter(read_time__range=(0,6)).order_by("read_time")
+    paginator = Paginator(texts, 1) # Show 1 text per page
+
+    page = request.GET.get('page')
+    try:
+        text = paginator.page(page)
+    except PageNotAnInteger:
+        text = paginator.page(1)
+    except EmptyPage:
+        text = paginator.page(paginator.num_pages)
+
+    return render(request, 'txct/text_display.html', {'text': text})
 
 def text_10(request):
-    texts = Text.objects.filter(Q(read_time__gt=6) & Q(read_time__lte=12)).order_by("?")[0]
-    return render(request, 'txct/text_10.html', {'texts': texts})
+    texts = Text.objects.filter(read_time__range=(7,12)).order_by("read_time")
+    paginator = Paginator(texts, 1) # Show 1 text per page
+
+    page = request.GET.get('page')
+    try:
+        text = paginator.page(page)
+    except PageNotAnInteger:
+        text = paginator.page(1)
+    except EmptyPage:
+        text = paginator.page(paginator.num_pages)
+
+    return render(request, 'txct/text_display.html', {'text': text})
 
 def text_15(request):
-    texts = Text.objects.filter(Q(read_time__gt=12) & Q(read_time__lte=16)).order_by("?")[0]
-    return render(request, 'txct/text_15.html', {'texts': texts})
+    texts = Text.objects.filter(read_time__range=(13,17)).order_by("read_time")
+    paginator = Paginator(texts, 1) # Show 1 text per page
+
+    page = request.GET.get('page')
+    try:
+        text = paginator.page(page)
+    except PageNotAnInteger:
+        text = paginator.page(1)
+    except EmptyPage:
+        text = paginator.page(paginator.num_pages)
+
+    return render(request, 'txct/text_display.html', {'text': text})
 
 def text_20(request):
-    texts = Text.objects.filter(Q(read_time__gt=16) & Q(read_time__lte=22)).order_by("?")[0]
-    return render(request, 'txct/text_20.html', {'texts': texts})
+    texts = Text.objects.filter(read_time__range=(18,22)).order_by("read_time")
+    paginator = Paginator(texts, 1) # Show 1 text per page
+
+    page = request.GET.get('page')
+    try:
+        text = paginator.page(page)
+    except PageNotAnInteger:
+        text = paginator.page(1)
+    except EmptyPage:
+        text = paginator.page(paginator.num_pages)
+
+    return render(request, 'txct/text_display.html', {'text': text})
 
 def text_25(request):
-    texts = Text.objects.filter(Q(read_time__gt=22) & Q(read_time__lte=35)).order_by("?")[0]
-    return render(request, 'txct/text_25.html', {'texts': texts})
+    texts = Text.objects.filter(read_time__range=(23,30)).order_by("read_time")
+    paginator = Paginator(texts, 1) # Show 1 text per page
+
+    page = request.GET.get('page')
+    try:
+        text = paginator.page(page)
+    except PageNotAnInteger:
+        text = paginator.page(1)
+    except EmptyPage:
+        text = paginator.page(paginator.num_pages)
+
+    return render(request, 'txct/text_display.html', {'text': text})
